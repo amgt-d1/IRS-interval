c7a37f1c79759f64a1ef10d4e6ec4f521765e6fa
Daichi Amagata <46043978+amgt-d1@users.noreply.github.com>
2023-07-20 09:59:27 +0900

    Update main.cpp

--- source/kds/main.cpp
+++ source/kds/main.cpp
@@ -51,7 +51,7 @@ int main()
     time_total_aggregate /= query_size;
     time_range_search_average /= query_size;
     time_range_sampling_average /= query_size;
-    std::cout << " Average IQS time: " << time_total_aggregate / 1000 << "[millisec]\t Throughput: " << 1.0 / (time_total_aggregate / 1000000) << "[queries/sec]\n\n";
+    std::cout << " Average IRS time: " << time_total_aggregate / 1000 << "[millisec]\t Throughput: " << 1.0 / (time_total_aggregate / 1000000) << "[queries/sec]\n\n";
 
     return 0;
 }
